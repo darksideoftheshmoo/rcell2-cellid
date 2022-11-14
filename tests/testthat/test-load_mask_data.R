8ede57ee5f083108b2f69ed6fec06da779f09473
naikymen <nmendez.ar@gmail.com>
2022-11-14 17:50:27 -0300

    write a test

--- tests/testthat/test-load_mask_data.R
+++ tests/testthat/test-load_mask_data.R
@@ -2,17 +2,28 @@ test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
 
-test_that("load_tsv_masks: load from examples", {
+test_that("read_coords_tsv works on examples", {
   # Example: Choose one set of example images:
-  data.dir <- system.file("extdata/sample_datasets/sample_time_series/Position4/out_all_masks.tsv.gz",
+  data.dir <- system.file("extdata/sample_datasets/sample_time_series/",
                           package = "rcell2.examples")
-  # Load
-  d <- read_coords_tsv(masks_tsv_path = data.dir)
+  # Load X
+  # X <- cell.load.alt(path = data.dir)
+  # saveRDS(X, "inst/extdata/test_data/cell.load.alt.RDS")  # Save X ref
   
-  # Load original
-  ref.tsv <- system.file("extdata/test_data/out_all_masks.RDS",
-                         package = "rcell2.examples")
+  # Load X ref
+  ref.X <- system.file("extdata/test_data/cell.load.alt.RDS",
+                       package = "rcell2.cellid")
+  X <- readRDS(ref.X)
+  
+  # Load Y
+  Y <- cell.load.boundaries(data.source = "masks.tsv", positions = X$positions)
+  # saveRDS(Y, "inst/extdata/test_data/cell.load.boundaries.RDS")  # Save Y ref
+  
+  # Load Y ref
+  Y.ref <- system.file("extdata/test_data/cell.load.boundaries.RDS",
+                         package = "rcell2.cellid")
+  Y.ref <- readRDS(Y.ref)
   
   # Test
-  expect_equal(d, ref.tsv)
+  expect_equal(Y, Y.ref)
 })
