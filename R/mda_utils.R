708033395124e50bf7d3a377cf81e6a0bad29e8a
naikymen <you@example.com>
2024-07-12 18:02:12 -0300

    adjust_stg_z_plot: adjust STG list Z by a reference calibration STG using a linear model

--- R/mda_utils.R
+++ R/mda_utils.R
@@ -42,7 +42,8 @@
 #' A template XLSX file can be obtained with \code{get_spos_template}.
 #' 
 #' @param spreadsheet_path Path to the an XLSX spreadsheet (e.g. "stage_positions.xlsx") with the following sheets: "pdata", "well_order", "well_images." A template file can be obtained with \code{get_spos_template}.
-#' @param spreadsheet_path Path where the STG file will be saved. This file is meant to be imported with MetaMorph's MDA.
+#' @param stg_output_path Path where the STG file will be saved. This file is meant to be imported with MetaMorph's MDA.
+#' @param calib_stg_list Path where the calibration STG can be found saved. NULL by default (disabled).
 #' @param well_sep Center-to-center distance between wells in the plate (in microns).
 #' @param well_width Width of the bottom of the wells (in microns).
 #' @param fov_width Width of the field of view of the final image (in microns).
@@ -56,6 +57,8 @@
 make_stage_list <- function(
   spreadsheet_path,
   stg_output_path="stage_list.STG",
+  print_output=FALSE,
+  calib_stg_list=NULL,
   well_sep = 4500,
   well_width = 3300,
   fov_width=500,
@@ -128,6 +131,48 @@ make_stage_list <- function(
     ) |> 
     ungroup()
   
+  # Adjust with calibration
+  if(!is.null(calib_stg_list)){
+    # Get the default calibration file.
+    if(isTRUE(calib_stg_list)){
+      calib_stg_list <- system.file("stage_positions-calib_tilt_B2_top_left.STG", package = "rcell2.cellid")
+    }
+    # Adjust the Z coordinate.
+    warning(paste("Adjusting stage coords with calibration file:", calib_stg_list))
+    stage_coords <- adjust_stg_z(stage_coords, plot_error = F)
+  }
+  
+  plt <- plot_stg_coords(stage_coords, origin_at_corner, origin_at_pos)
+  
+  stg_file_header <- paste(
+    '"Stage Memory List", Version 6.0',
+    '0, 0, 0, 0, 0, 0, 0, "um", "um"',
+    '0',
+    nrow(positions),
+    sep = "\n"
+  )
+  
+  # Make output dataframe.
+  stg_file_data <- make_stage_coods(stage_coords)
+  
+  # Write the output.
+  write(x = stg_file_header, file = stg_output_path, append = F)
+  write.table(stg_file_data, sep = ", ", file=stg_output_path, col.names = F, append = T, row.names = F)
+  
+  # Log.
+  cat(paste0("Wrote the STG file to: ", normalizePath(stg_output_path), "\n"))
+  if(print_output){
+    readLines(stg_output_path) |> paste(collapse = "\n") |> cat()
+  }
+
+  return(list(
+    stg_output_path=stg_output_path,
+    positions_plot=plt, 
+    stage_coords=stage_coords
+  ))
+}
+
+plot_stg_coords <- function(stage_coords, origin_at_corner, origin_at_pos){
   plt <- 
     stage_coords |> 
     mutate(
@@ -149,13 +194,10 @@ make_stage_list <- function(
               paste0(origin_at_corner, " corner, at position ", origin_at_pos, ").")) +
     theme_minimal()
   
-  stg_file_header <- paste(
-    '"Stage Memory List", Version 6.0',
-    '0, 0, 0, 0, 0, 0, 0, "um", "um"',
-    '0',
-    nrow(positions),
-    sep = "\n"
-  )
+  return(plt)
+}
+
+make_stage_coods <- function(stage_coords){
   stg_file_data <- stage_coords |> 
     arrange(pos) |> 
     select(pos, x, y, z, af_offset) |> 
@@ -170,15 +212,78 @@ make_stage_list <- function(
       v5 = -1,
       v6 = ""
     )
-  write(x = stg_file_header, file = stg_output_path, append = F)
-  write.table(stg_file_data, sep = ", ", file=stg_output_path, col.names = F, append = T, row.names = F)
   
-  cat(paste0("Wrote the following content to: ", stg_output_path, "\n"))
-  readLines(stg_output_path) |> paste(collapse = "\n") |> cat()
-
-  return(list(
-    stg_output_path=stg_output_path,
-    positions_plot=plt, 
-    stage_coords=stage_coords
-  ))
+  return(stg_file_data)
 }
+
+#' Read STG file
+#' 
+#' @import ggplot2
+read_stg <- function(stg_list_path, plot_stage=TRUE){
+  stg_list <- stg_list_path |> 
+    read.csv(skip = 4, header = F)
+  
+  stg_list <- stg_list[1:5]
+  
+  names(stg_list) <- c(
+    "position_name",
+    "x", "y", "z",
+    "af_offset"
+  )
+  
+  if(plot_stage){
+    plt <- stg_list |> 
+      ggplot() +
+      geom_path(aes(x,y)) +
+      geom_point(aes(x,y,color=z), size=10, alpha =.5) +
+      scale_color_viridis_c() + theme_minimal()
+    print(plt)
+  }
+  
+  return(stg_list)
+}
+
+model_stg_z <- function(calib_stg_list){
+  m <- lm(z~x+y, data=calib_stg_list)
+  return(m)
+}
+
+#' Use a calibration STG file to adjust the Z coordinates of a "stage_coords" data frame
+#' @param stage_coords Stage coordinates data frame, as produced by "make_stage_list".
+#' @param calib_stg_path Path to an STG file with calibration points. Their origin must be the same as the origin used in \code{stage_coords}.
+#' @inheritParams make_stage_list
+#' @export
+adjust_stg_z <- function(
+    stage_coords,
+    calib_stg_path = system.file("stage_positions-calib_tilt_B2_top_left.STG", package = "rcell2.cellid"),
+    origin_at_corner = "top-right",
+    origin_at_pos = 1,
+    plot_error = FALSE
+){
+  if(origin_at_corner != "top-right") stop("Only top-right origins supported ATM.")
+  if(origin_at_pos != 1) stop("The origin can only be in the well of the first position ATM.")
+  
+  calib_stg_list <- read_stg(calib_stg_path, plot_stage = F)
+  m <- model_stg_z(calib_stg_list)
+  xy <- stage_coords[,c("x", "y")]
+  new_z <- predict(m, newdata=xy)
+  stage_coords$z_err <- new_z - stage_coords$z
+  stage_coords$z <- new_z
+  
+  if(plot_error){
+    stage_coords |> adjust_stg_z_plot() |> print()
+  }
+  
+  return(stage_coords)
+}
+
+adjust_stg_z_plot <- function(stage_coords){
+  plt <- stage_coords |> 
+    ggplot() +
+    geom_path(aes(x,y)) +
+    geom_point(aes(x,y,color=z_err), size=10, alpha =.5) +
+    scale_x_reverse() + 
+    scale_color_viridis_c() + theme_minimal()
+  return(plt)
+}
+
